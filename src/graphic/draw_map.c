07cee05f486eda4055e68b116fa197198ed88a9b
aautret <aautret@student.42.fr>
2026-01-15 17:53:18 +0100

    Affichage des rayons mini-map + doxygens

--- src/graphic/draw_map.c
+++ src/graphic/draw_map.c
@@ -3,15 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   draw_map.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/13 16:56:26 by tlorette          #+#    #+#             */
-/*   Updated: 2026/01/15 15:26:03 by tlorette         ###   ########.fr       */
+/*   Updated: 2026/01/15 17:38:32 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3D.h"
 
+/**
+ * @brief Place un pixel de couleur sur l'image aux coordonnees (x, y)
+ * 
+ * @param img 
+ * @param x 
+ * @param y 
+ * @param color 
+ */
 void	my_put_pixel(t_img *img, int x, int y, int color)
 {
 	int	offset;
@@ -22,6 +30,101 @@ void	my_put_pixel(t_img *img, int x, int y, int color)
 	*(int *)(img->addr + offset) = color;
 }
 
+/**
+ * @brief Dessine un rayon à angle négatif depuis la position du joueur
+ * (hypotenuse)
+ * 
+ * @param img 
+ * @param j 
+ */
+static void	draw_negative_angle(t_img *img, int j)
+{
+	double	step_x;
+	double	step_y;
+	double	current_x;
+	double	current_y;
+	int		i;
+
+	if (!img || !img->player)
+		return ;
+	img->player->view_angle = (-M_PI / 8);
+	img->distance_x = img->player->origin_x + 100
+		* cos(img->player->view_angle);
+	img->distance_y = img->player->origin_y + j + 100
+		* sin(img->player->view_angle);
+	step_x = (img->distance_x - img->player->origin_x) / ADJACENT;
+	step_y = (img->distance_y - img->player->origin_y) / ADJACENT;
+	i = 0;
+	while (i < ADJACENT)
+	{
+		current_x = img->player->origin_x + 9 + step_x * i;
+		current_y = img->player->origin_y + 9 + step_y * i;
+		my_put_pixel(img, (int)current_x, (int)current_y, 0xFFFF00);
+		i++;
+	}
+	// printf("%f\n%f\n", img->distance_x, img->distance_y);
+}
+
+/**
+ * @brief Dessine un rayona angle positif depuis a position du joueur 
+ * (hypotenus)
+ * 
+ * @param img 
+ * @param j 
+ */
+static void	draw_positive_angle(t_img *img, int j)
+{
+	double	step_x;
+	double	step_y;
+	double	current_x;
+	double	current_y;
+	int		i;
+
+	if (!img || !img->player)
+		return ;
+	img->player->view_angle = (M_PI / 8);
+	img->distance_x = img->player->origin_x + 100
+		* cos(img->player->view_angle);
+	img->distance_y = img->player->origin_y - j + 100
+		* sin(img->player->view_angle);
+	step_x = (img->distance_x - img->player->origin_x) / ADJACENT;
+	step_y = (img->distance_y - img->player->origin_y) / ADJACENT;
+	i = 0;
+	while (i < ADJACENT)
+	{
+		current_x = img->player->origin_x + 9 + step_x * i;
+		current_y = img->player->origin_y + 9 + step_y * i;
+		my_put_pixel(img, (int)current_x, (int)current_y, 0xFFFF00);
+		i++;
+	}
+	// printf("%f\n%f\n", img->distance_y, img->distance_x);
+}
+
+/**
+ * @brief Permet de dessiner tous les rayons de l'angle de vue sur la mini-map
+ * 
+ * @param img 
+ */
+static void	draw_angles(t_img *img)
+{
+	int	j;
+	int	height_1;
+
+	j = 0;
+	height_1 = calc_height(img);
+	while (j < height_1)
+	{
+		draw_negative_angle(img, j);
+		draw_positive_angle(img, j);
+		j++;
+	}
+}
+
+/**
+ * @brief Dessine le point initial du joueur et les rayons sur la mini-map
+ * 
+ * @param img 
+ */
 void	draw_initial_dot(t_img *img)
 {
 	int	x;
@@ -45,154 +148,6 @@ void	draw_initial_dot(t_img *img)
 		}
 		y++;
 	}
-	draw_negative_angle(img);
-	draw_positive_angle(img);
-	draw_adjacent(img);
-	draw_positive_height(img);
-	draw_negative_height(img);
+	draw_angles(img);
 	mlx_put_image_to_window(img->game->mlx, img->game->win, img->img, 0, 0);
 }
-
-void	draw_negative_angle(t_img *img)
-{
-	double	step_x;
-	double	step_y;
-	double	current_x;
-	double	current_y;
-	int		i;
-
-	if (!img || !img->player)
-		return ;
-	img->player->view_angle = (-M_PI / 8);
-	img->distance_x = img->player->origin_x + 100
-		* cos(img->player->view_angle);
-	img->distance_y = img->player->origin_y + 100
-		* sin(img->player->view_angle);
-	step_x = (img->distance_x - img->player->origin_x) / ADJACENT;
-	step_y = (img->distance_y - img->player->origin_y) / ADJACENT;
-	i = 0;
-	while (i < ADJACENT)
-	{
-		current_x = img->player->origin_x + 9 + step_x * i;
-		current_y = img->player->origin_y + 9 + step_y * i;
-		my_put_pixel(img, (int)current_x, (int)current_y, 0xFFFF00);
-		i++;
-	}
-	// printf("%f\n%f\n", img->distance_x, img->distance_y);
-}
-
-void	draw_positive_angle(t_img *img)
-{
-	double	step_x;
-	double	step_y;
-	double	current_x;
-	double	current_y;
-	int		i;
-
-	if (!img || !img->player)
-		return ;
-	img->player->view_angle = (M_PI / 8);
-	img->distance_x = img->player->origin_x + 100
-		* cos(img->player->view_angle);
-	img->distance_y = img->player->origin_y + 100
-		* sin(img->player->view_angle);
-	step_x = (img->distance_x - img->player->origin_x) / ADJACENT;
-	step_y = (img->distance_y - img->player->origin_y) / ADJACENT;
-	i = 0;
-	while (i < ADJACENT)
-	{
-		current_x = img->player->origin_x + 9 + step_x * i;
-		current_y = img->player->origin_y + 9 + step_y * i;
-		my_put_pixel(img, (int)current_x, (int)current_y, 0xFFFF00);
-		i++;
-	}
-	// printf("%f\n%f\n", img->distance_y, img->distance_x);
-}
-
-void	draw_adjacent(t_img *img)
-{
-	double	step_x;
-	double	step_y;
-	double	current_x;
-	double	current_y;
-	int		i;
-
-	if (!img || !img->player)
-		return ;
-	img->player->view_angle = (-M_PI / 8);
-	img->distance_x = img->player->origin_x + 100
-		* cos(img->player->view_angle);
-	step_x = (img->distance_x - img->player->origin_x) / ADJACENT;
-	step_y = (img->distance_y - img->player->origin_y) / ADJACENT;
-	i = 0;
-	while (i < ADJACENT)
-	{
-		current_x = img->player->origin_x + 9 + step_x * i;
-		current_y = img->player->origin_y + 9;
-		my_put_pixel(img, (int)current_x, (int)current_y, 0xFFFF00);
-		i++;
-	}
-}
-
-void	draw_negative_height(t_img *img)
-{
-	double	step_x;
-	double	step_y;
-	double	current_x;
-	double	current_y;
-	int		i;
-
-	if (!img || !img->player)
-		return ;
-	img->player->view_angle = (-M_PI / 8);
-	img->distance_x = img->player->origin_x + 100
-		* cos(img->player->view_angle);
-	step_x = (img->distance_x - img->player->origin_x) / ADJACENT;
-	step_y = (img->distance_y - img->player->origin_y) / ADJACENT;
-	i = 0;
-	while (i < ADJACENT)
-	{
-		current_x = img->player->origin_x + ADJACENT;
-		current_y = img->player->origin_y + 9 + step_y * i;
-		my_put_pixel(img, (int)current_x, (int)current_y, 0xFFFF00);
-		i++;
-	}
-}
-
-void	draw_positive_height(t_img *img)
-{
-	double	step_x;
-	double	step_y;
-	double	current_x;
-	double	current_y;
-	int		i;
-
-	if (!img || !img->player)
-		return ;
-	img->player->view_angle = (M_PI / 8);
-	img->distance_x = img->player->origin_x + 100
-		* cos(img->player->view_angle);
-	step_x = (img->distance_x - img->player->origin_x) / ADJACENT;
-	step_y = (img->distance_y - img->player->origin_y) / ADJACENT;
-	i = 0;
-	while (i < ADJACENT)
-	{
-		current_x = img->player->origin_x + ADJACENT;
-		current_y = img->player->origin_y + 9 - step_y * i;
-		my_put_pixel(img, (int)current_x, (int)current_y, 0xFFFF00);
-		i++;
-	}
-}
-
-// void	fill_angle(t_img *img)
-// {
-// 	double	current_y;
-
-// 	current_y = img->distance_y;
-// 	while (current_y > img->player->origin_y + 9)
-// 	{
-// 		current_y = img->distance_y;
-// 		my_put_pixel(img, img->distance_x, current_y, 0xFFFF00);
-// 		img->distance_y--;
-// 	}
-// }
