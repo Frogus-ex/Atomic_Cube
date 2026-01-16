911739571afb56103472f3a0b8edb0809147d04c
aautret <aautret@student.42.fr>
2026-01-16 11:48:46 +0100

    Parsing map alemiorer avec le checker de player

--- src/parsing/parsing_map.c
+++ src/parsing/parsing_map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing_map.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 11:20:20 by aautret           #+#    #+#             */
-/*   Updated: 2026/01/16 11:17:21 by tlorette         ###   ########.fr       */
+/*   Updated: 2026/01/16 11:46:36 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -127,15 +127,19 @@
  * @param line
  * @return int
  */
-int	parse_map_line(char *line)
+int	parse_map_line(t_game *game, char *line)
 {
-	int		i;
+	int	i;
 
 	i = 0;
 	while (line[i])
 	{
 		if (!ft_strchr("10NSEW \n", line[i]))
-			return (printf("error: invalid caracter detected\n"), 1);
+			return (printf("error: invalid caracter detected \n"), 1);
+		if (ft_strchr("NSEW", line[i]) && game->flag_players == 1)
+			return (printf("error: invalid number of players in map\n"), 1);
+		else if (ft_strchr("NSEW", line[i]) && game->flag_players == 0)
+			game->flag_players = 1;
 		i++;
 	}
 	return (0);
