296b791fc3efddbf6c1c2b4c2908b7f23e730bfa
aautret <aautret@student.42.fr>
2026-01-17 15:17:28 +0100

    Initialisatin de la vue du player

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 13:18:39 by tlorette          #+#    #+#             */
-/*   Updated: 2026/01/16 16:52:46 by tlorette         ###   ########.fr       */
+/*   Updated: 2026/01/21 10:51:44 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int	main(int ac, char **av)
 	img_init(&img);
 	if (!img)
 		return (free(map), free_all(game), 1);
-	player_init(&player);
+	player_init(&player, game);
 	if (!player)
 		return (free(map), free_all(game), 1);
 	if (parsing(game, av[1]))
