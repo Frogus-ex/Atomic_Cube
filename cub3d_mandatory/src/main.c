7f0231f7f7d669660071ac6aef9be61dbdd6aa76
TOM <tomlorette2703@gmail.com>
2026-01-30 10:35:18 +0100

    fonction pour exterioriser les initialisation

--- cub3d_mandatory/src/main.c
+++ cub3d_mandatory/src/main.c
@@ -3,59 +3,70 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/17 13:18:39 by tlorette          #+#    #+#             */
-/*   Updated: 2026/01/29 17:50:42 by aautret          ###   ########.fr       */
+/*   Updated: 2026/01/30 10:19:06 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3D.h"
 
-int	main(int ac, char **av)
+t_game	*init_all(void)
 {
 	t_game		*game;
 	t_map		*map;
 	t_img		*img;
-	t_player	*player;
 	t_texture	*texture;
+
+	game = (t_game *)malloc(sizeof(t_game));
+	if (!game)
+		return (NULL);
+	ft_bzero(game, sizeof(t_game));
+	map = (t_map *)malloc(sizeof(t_map));
+	if (!map)
+		return (free(game), NULL);
+	ft_bzero(map, sizeof(t_map));
+	game->map = map;
+	texture = (t_texture *)malloc(sizeof(t_texture));
+	if (!texture)
+		return (free_all(game), NULL);
+	ft_bzero(texture, sizeof(t_texture));
+	game->textures = texture;
+	img = (t_img *)malloc(sizeof(t_img));
+	if (!img)
+		return (free_all(game), NULL);
+	ft_bzero(img, sizeof(t_img));
+	game->img = img;
+	return (game);
+}
+
+int	main(int ac, char **av)
+{
+	t_game	*game;
 
 	if (ac != 2)
 		return (ft_error(NULL, "wrong number of args"), 1);
-	game_init(&game);
+	game = init_all();
 	if (!game)
-		return (1);
-	map_init(&map);
-	if (!map)
-		return (free(game), 1);
-	game->map = map;
-	text_init(&texture);
-	if (!texture)
-		return (free_all(game), 1);
-	game->textures = texture;
-	img_init(&img, game);
-	if (!img)
-		return (free_all(game), 1);
-	game->img = img;
-	player_init(&player, game);
-	if (!player)
-		return (free_all(game), 1);
+		return (ft_error(NULL, "Failed to initialize game"), 1);
 	if (parsing(game, av[1]))
 		return (free_all(game), 1);
-	read_from_map(game, map, av[1]);
-	if (flood_fill(map))
+	read_from_map(game, game->map, av[1]);
+	if (flood_fill(game->map))
 		return (free_all(game), 1);
-	img->game = game;
-	img->map = map;
-	img->player = player;
-	// initialize player origin in pixels from map start position
-	img->player->origin_x = img->map->player_x * TILE_SIZE + TILE_SIZE / 2;
-	img->player->origin_y = img->map->player_y * TILE_SIZE + TILE_SIZE / 2;
+	game->img->game = game;
+	game->img->map = game->map;
+	game->img->player = game->player;
+	game->img->player->origin_x = game->map->player_x * TILE_SIZE + TILE_SIZE
+		/ 2;
+	game->img->player->origin_y = game->map->player_y * TILE_SIZE + TILE_SIZE
+		/ 2;
 	if (!check_colors_param(game))
 		return (ft_error(game, "colors param are not in RGB format"), 0);
-	if (!init_mlx(game, map, img))
+	if (!init_mlx(game, game->map, game->img))
 		return (0);
-	put_cub3d_to_wnd(img);
+	put_cub3d_to_wnd(game->img);
 	mlx_loop(game->mlx);
 	return (free_all(game), 0);
 }
